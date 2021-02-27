37848b28e2a84318ba1f7dd4e2aec22e58de2e15
adriano.ament <adrianjoament@gmail.com>
2021-02-27 15:06:12 -0300

    Adicionando classe de teste.

---
+++ backend/src/api.data.test/RecursoCrudCompleto.cs
@@ -0,0 +1,85 @@
+﻿using api.data.Context;
+using api.data.Repositories;
+using api.domain.Entities;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace api.data.test
+{
+    public class RecursoCrudCompleto : BaseTest, IClassFixture<DbTeste>
+    {
+        private ServiceProvider _serviceProvider;
+        public RecursoCrudCompleto(DbTeste dbTeste)
+        {
+            _serviceProvider = dbTeste.serviceProvider;
+        }
+
+        [Fact(DisplayName = "CRUD de Recursos")]
+        [Trait("CRUD", "RecursosEntity")]
+        public async Task E_PossivelRealizar_CRUD()
+        {
+            using (var context = _serviceProvider.GetService<VotosContext>())
+            {
+                RecursosRepository repository = new RecursosRepository(context);
+                RecursoEntity entity = new RecursoEntity()
+                {
+                    Tarefa = Faker.Lorem.Sentence(3)                    
+                };
+                var novoRegistro = await E_PossivelRealizar_Insercao(repository, entity);
+
+                entity.Id = novoRegistro.Id;
+                entity.Tarefa = Faker.Lorem.Sentence(3);
+                novoRegistro = await E_PossivelRealizar_Atualizacao(repository, entity);
+                                
+                await Existe_Id(repository, novoRegistro);
+                await Tem_Varios(repository);
+
+                await E_PossivelExcluir(repository, novoRegistro);
+            }
+        }
+
+        private async Task Tem_Varios(RecursosRepository usuariosRepository)
+        {
+            var _registros = await usuariosRepository.SelectAsync();
+            Assert.NotNull(_registros);
+            Assert.True(_registros.Count() > 0);
+        }
+
+        private async Task Existe_Id(RecursosRepository usuariosRepository, RecursoEntity entity)
+        {
+            var _registro = await usuariosRepository.SelectAsync(entity.Id);
+            Assert.NotNull(_registro);
+            Assert.Equal(entity.Tarefa, _registro.Tarefa);
+            Assert.False(entity.Id == Guid.Empty);
+        }
+
+        private async Task E_PossivelExcluir(RecursosRepository repository, RecursoEntity entity)
+        {
+            var sucesso = await repository.DeleteAsync(entity.Id);
+            Assert.True(sucesso);
+        }
+
+        private async Task<RecursoEntity> E_PossivelRealizar_Atualizacao(RecursosRepository repository, RecursoEntity entity)
+        {
+            var _registro = await repository.UpdateAsync(entity);
+            Assert.NotNull(_registro);
+            Assert.Equal(entity.Tarefa, _registro.Tarefa);
+            Assert.False(entity.Id == Guid.Empty);
+            return _registro;
+        }
+
+        private static async Task<RecursoEntity> E_PossivelRealizar_Insercao(RecursosRepository repository, RecursoEntity entity)
+        {
+            var _registro = await repository.InsertAsync(entity);
+            Assert.NotNull(_registro);
+            Assert.Equal(entity.Tarefa, _registro.Tarefa);            
+            Assert.False(entity.Id == Guid.Empty);
+            return _registro;
+        }
+    }
+}
---
+++ backend/src/api.data.test/UsuarioCrudCompleto.cs
@@ -0,0 +1,105 @@
+﻿using api.data.Context;
+using api.data.Repositories;
+using api.domain.Entities;
+using LanguageExt.UnitsOfMeasure;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace api.data.test
+{
+    public class UsuarioCrudCompleto : BaseTest, IClassFixture<DbTeste>
+    {
+        private ServiceProvider _serviceProvider;
+
+        public UsuarioCrudCompleto(DbTeste dbTeste)
+        {
+            _serviceProvider = dbTeste.serviceProvider;
+        }
+
+        [Fact(DisplayName ="CRUD de Usuario")]
+        [Trait("CRUD", "UsuarioEntity")]
+        public async Task E_PossivelRealizar_CRUD_Usuario()
+        {
+            using (var context = _serviceProvider.GetService<VotosContext>())
+            {
+                UsuariosRepository usuariosRepository = new UsuariosRepository(context);
+                UsuarioEntity entity = new UsuarioEntity()
+                {
+                    eMail = Faker.Internet.Email(),
+                    Nome = Faker.Name.FullName(),
+                    Senha = Guid.NewGuid().ToString().Replace("-", string.Empty).Substring(0, 10)
+                };
+                var newUsuario = await CriarUsuario(usuariosRepository, entity);
+            
+                entity.Id = newUsuario.Id;
+                entity.Nome = Faker.Name.FullName();
+                newUsuario = await E_PossivelRealizar_Insercao(usuariosRepository, entity);
+
+                await E_Possivel_Procurar_Email(usuariosRepository, newUsuario);
+                await Existe_Id(usuariosRepository, newUsuario);
+                await Tem_Varios(usuariosRepository);
+
+                await E_PossivelExcluir(usuariosRepository, newUsuario);                
+            }
+        }
+
+        private async Task Tem_Varios(UsuariosRepository usuariosRepository)
+        {
+            var _registros = await usuariosRepository.SelectAsync();
+            Assert.NotNull(_registros);
+            Assert.True(_registros.Count() > 0);
+        }
+
+        private async Task Existe_Id(UsuariosRepository usuariosRepository, UsuarioEntity entity)
+        {
+            var _registro = await usuariosRepository.SelectAsync(entity.Id);
+            Assert.NotNull(_registro);
+            Assert.Equal(entity.eMail, _registro.eMail);
+            Assert.Equal(entity.Senha, _registro.Senha);
+            Assert.Equal(entity.Nome, _registro.Nome);
+            Assert.False(entity.Id == Guid.Empty);
+        }
+
+        private async Task E_PossivelExcluir(UsuariosRepository usuariosRepository, UsuarioEntity newUsuario)
+        {
+            var sucesso = await usuariosRepository.DeleteAsync(newUsuario.Id);
+            Assert.True(sucesso);
+        }
+
+        private async Task E_Possivel_Procurar_Email(UsuariosRepository usuariosRepository, UsuarioEntity entity)
+        {
+            var _registro = await usuariosRepository.getEmail(entity.eMail);
+            Assert.NotNull(_registro);
+            Assert.Equal(entity.eMail, _registro.eMail);
+            Assert.Equal(entity.Senha, _registro.Senha);
+            Assert.Equal(entity.Nome, _registro.Nome);
+            Assert.False(entity.Id == Guid.Empty);
+        }
+
+        
+        private async Task<UsuarioEntity> E_PossivelRealizar_Insercao(UsuariosRepository usuariosRepository, UsuarioEntity entity)
+        {
+            var _registroCriado = await usuariosRepository.UpdateAsync(entity);
+            Assert.NotNull(_registroCriado);
+            Assert.Equal(entity.eMail, _registroCriado.eMail);
+            Assert.False(entity.Id == Guid.Empty);
+            return _registroCriado;
+        }
+                
+
+        private static async Task<UsuarioEntity> CriarUsuario(UsuariosRepository usuariosRepository, UsuarioEntity entity)
+        {
+            var _registro = await usuariosRepository.InsertAsync(entity);
+            Assert.NotNull(_registro);
+            Assert.Equal(entity.eMail, _registro.eMail);
+            Assert.Equal(entity.Senha, _registro.Senha);
+            Assert.False(entity.Id == Guid.Empty);
+            return _registro;
+        }
+    }
+}
